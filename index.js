3f21c0048ef350c4d097820e9abf3d06f01b2b69
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2018-01-31 00:27:12 +0100

    players score

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const Game = () => {
-    const score = {
+    let score = {
         playerA: 0,
         playerB: 0,
     };
 
     const displayScore = () => `${score.playerA} - ${score.playerB}`;
 
+    const getNewScore = {
+        0: 15,
+        15: 30,
+        30: 40,
+        40: 0,
+    };
+
+    const playerScores = (player) => {
+        score[player] = getNewScore[score[player]];
+    };
+
     return {
         displayScore,
+        playerScores,
     }
 };
 
--- index.test.js
+++ index.test.js
@@ -11,4 +11,15 @@ describe('A tennis game', () => {
     it('score is 0 - 0 when it is about to start', () => {
         expect(game.displayScore()).to.eql('0 - 0');
     });
+
+    it('player A scores => 15 - 0', () => {
+        game.playerScores('playerA');
+        expect(game.displayScore()).to.eql('15 - 0');
+    });
+
+    it('tied to 15-all', () => {
+        game.playerScores('playerA');
+        game.playerScores('playerB');
+        expect(game.displayScore()).to.eql('15 - 15');
+    });
 });
