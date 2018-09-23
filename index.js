6717bd51ca86f0fa58e3223056b6bf8c61b566cf
Carlos Jiménez <carlos.jimenez@guidesmiths.com>
2018-09-23 23:50:33 +0200

    Clean code and set indent to 2

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
 const Game = () => {
-    let score = {
-        playerA: 0,
-        playerB: 0,
+  let score = {
+    playerA: 0,
+    playerB: 0,
+  };
+
+  const formattedScore = (_score_) => ({
+    '40 - 40': 'DEUCE',
+    '50 - 40': 'ADV PLAYER A',
+    '40 - 50': 'ADV PLAYER B',
+  })[_score_] || _score_;
+
+  const displayScore = () => formattedScore(`${score.playerA} - ${score.playerB}`);
+
+  const getNewScore = {
+    0: 15,
+    15: 30,
+    30: 40,
+    40: 50,
+  };
+
+  const playerScores = (player) => score[player] = getNewScore[score[player]];
+
+  const checkPoints = (points) => [0, 15, 30, 40].indexOf(points) > -1;
+  const setScore = (_score_) => {
+    [playerAPoints, playerBPoints] = _score_.split('-').map(x => parseInt(x));
+    score = {
+      playerA: checkPoints(playerAPoints) ? playerAPoints : 999,
+      playerB: checkPoints(playerBPoints) ? playerBPoints : 999,
     };
+  };
 
-    const formattedScore = (_score_) => ({
-        '40 - 40': 'DEUCE',
-        '50 - 40': 'ADV PLAYER A',
-        '40 - 50': 'ADV PLAYER B',
-    })[_score_] || _score_;
-
-    const displayScore = () => formattedScore(`${score.playerA} - ${score.playerB}`);
-
-    const getNewScore = {
-        0: 15,
-        15: 30,
-        30: 40,
-        40: 50,
-    };
-
-    const playerScores = (player) => {
-        score[player] = getNewScore[score[player]];
-    };
-
-    const checkPoints = (points) => [0, 15, 30, 40].indexOf(points) > -1;
-    const setScore = (_score_) => {
-        [playerAPoints, playerBPoints] = _score_.split('-').map(x => parseInt(x));
-        score = {
-            playerA: checkPoints(playerAPoints) ? playerAPoints : 999,
-            playerB: checkPoints(playerBPoints) ? playerBPoints : 999,
-        };
-    };
-
-    return {
-        displayScore,
-        playerScores,
-        setScore,
-    };
+  return {
+    displayScore,
+    playerScores,
+    setScore,
+  };
 };
 
 module.exports = Game;
--- index.test.js
+++ index.test.js
@@ -4,47 +4,47 @@ const Game = require('./index');
 let game;
 
 beforeEach(() => {
-    game = Game();
+  game = Game();
 });
 
 describe('A tennis game', () => {
-    it('score is 0 - 0 when it is about to start', () => {
-        expect(game.displayScore()).to.eql('0 - 0');
-    });
+  it('score is 0 - 0 when it is about to start', () => {
+    expect(game.displayScore()).to.eql('0 - 0');
+  });
 
-    it('player A scores => 15 - 0', () => {
-        game.playerScores('playerA');
-        expect(game.displayScore()).to.eql('15 - 0');
-    });
+  it('player A scores => 15 - 0', () => {
+    game.playerScores('playerA');
+    expect(game.displayScore()).to.eql('15 - 0');
+  });
 
-    it('tied to 15-all', () => {
-        game.playerScores('playerA');
-        game.playerScores('playerB');
-        expect(game.displayScore()).to.eql('15 - 15');
-    });
+  it('tied to 15-all', () => {
+    game.playerScores('playerA');
+    game.playerScores('playerB');
+    expect(game.displayScore()).to.eql('15 - 15');
+  });
 
-    it('score can be set', () => {
-        game.setScore('0 - 0');
-        expect(game.displayScore()).to.eql('0 - 0');
-        game.setScore('15 - 0');
-        expect(game.displayScore()).to.eql('15 - 0');
-        game.setScore('30 - 15');
-        expect(game.displayScore()).to.eql('30 - 15');
-        game.setScore('40 - 30');
-        expect(game.displayScore()).to.eql('40 - 30');
-        game.setScore('sfdffs');
-        expect(game.displayScore()).to.eql('999 - 999');
-    })
+  it('score can be set', () => {
+    game.setScore('0 - 0');
+    expect(game.displayScore()).to.eql('0 - 0');
+    game.setScore('15 - 0');
+    expect(game.displayScore()).to.eql('15 - 0');
+    game.setScore('30 - 15');
+    expect(game.displayScore()).to.eql('30 - 15');
+    game.setScore('40 - 30');
+    expect(game.displayScore()).to.eql('40 - 30');
+    game.setScore('sfdffs');
+    expect(game.displayScore()).to.eql('999 - 999');
+  })
 
-    it('DEUCE', () => {
-        game.setScore('40-30');
-        game.playerScores('playerB');
-        expect(game.displayScore()).to.eql('DEUCE');
-    });
+  it('DEUCE', () => {
+    game.setScore('40-30');
+    game.playerScores('playerB');
+    expect(game.displayScore()).to.eql('DEUCE');
+  });
 
-    it('player B gets Advantage', () => {
-        game.setScore('40-40');
-        game.playerScores('playerB');
-        expect(game.displayScore()).to.eql('ADV PLAYER B');
-    })
+  it('player B gets Advantage', () => {
+    game.setScore('40-40');
+    game.playerScores('playerB');
+    expect(game.displayScore()).to.eql('ADV PLAYER B');
+  });
 });
